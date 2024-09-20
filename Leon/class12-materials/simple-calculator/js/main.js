e2bb162d0763e4d08cc6806e17ad73dda263b6e2
Lavell Anthony Wesley <lavellthedev+github@gmail.com>
2024-09-19 23:38:54 -0400

    class 12 complete

--- Leon/class12-materials/simple-calculator/js/main.js
+++ Leon/class12-materials/simple-calculator/js/main.js
@@ -1,32 +1,67 @@
+// let total = 0
+
+// document.querySelector('#pumpkin').addEventListener('click', makeZero)
+// document.querySelector('#dominosPizza').addEventListener('click', jumanji)
+// document.querySelector('#zebra').addEventListener('click', add9)
+// document.querySelector('#cantThinkOfAnything').addEventListener('click', sub2)
+// document.querySelector("#minusThree").addEventListener('click', sub3)
+
+// function makeZero() {
+//   total = 0
+//   document.querySelector('#placeToPutResult').innerText = total
+// }
+
+// function jumanji() {
+//   total = total + 3
+//   document.querySelector('#placeToPutResult').innerText = total
+// }
+
+// function add9() {
+//   total = total + 9
+//   document.querySelector('#placeToPutResult').innerHTML = total
+// }
+
+// function sub2() {
+//   total = total - 2
+//   document.querySelector('#placeToPutResult').innerHTML = total
+// }
+
+// function sub3() {
+//   total = total - 3
+//   document.querySelector('#placeToPutResult').innerHTML = total
+// }
+
+
+
 let total = 0
 
-document.querySelector('#pumpkin').addEventListener('click', makeZero)
+document.getElementById('pumpkin').onclick = makeZero
 document.querySelector('#dominosPizza').addEventListener('click', jumanji)
-document.querySelector('#zebra').addEventListener('click', add9)
-document.querySelector('#cantThinkOfAnything').addEventListener('click', sub2)
-document.querySelector("#minusThree").addEventListener('click', sub3)
+document.querySelector('#zebra').addEventListener('click', stripes)
+document.getElementById('cantThinkOfAnything').onclick = cantThink
+document.querySelector('#minusThree').addEventListener('click', removeThree)
 
-function makeZero() {
+function makeZero (){
   total = 0
-  document.querySelector('#placeToPutResult').innerText = total
-}
-
-function jumanji() {
-  total = total + 3
-  document.querySelector('#placeToPutResult').innerText = total
-}
-
-function add9() {
-  total = total + 9
   document.querySelector('#placeToPutResult').innerHTML = total
 }
 
-function sub2() {
-  total = total - 2
-  document.querySelector('#placeToPutResult').innerHTML = total
+function jumanji (){
+  total += 3
+  document.querySelector('#placeToPutResult').innerHTML = total;
 }
 
-function sub3() {
-  total = total - 3
-  document.querySelector('#placeToPutResult').innerHTML = total
+function stripes(){
+  total += 9 
+  document.querySelector('#placeToPutResult').innerHTML = total;
+}
+
+function cantThink(){
+  total -= 2 
+  document.querySelector('#placeToPutResult').innerHTML = total;
+}
+
+function removeThree(){
+  total -= 3 
+  document.querySelector('#placeToPutResult').innerHTML = total;
 }
