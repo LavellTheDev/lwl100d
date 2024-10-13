564fb93e2b07bff3568301fa10927db7677aafd8
Lavell Anthony Wesley <lavellthedev+github@gmail.com>
2024-10-13 15:12:55 -0400

    40% complete with lesson 13

--- Leon/class13-materials/temperature-converter/js/main.js
+++ Leon/class13-materials/temperature-converter/js/main.js
@@ -1,1 +1,16 @@
 //Write your pseduo code first! 
+
+// I need to convert temperature from C to F. 
+
+// I need a way to display the change in the DOM. 
+document.querySelector('#yell').addEventListener('click', convert)
+
+function convert() {
+
+    //Need the valus of Cel 
+    let temp = document.querySelector('#cel').value
+    //convert cel to farhn
+    temp = temp * 9/5 + 32
+    //show it
+    document.querySelector('#placeToYell').innerText = temp
+  }
