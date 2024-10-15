234229897438aaf227cfe90b92f4e0843f82610c
Lavell Anthony Wesley <lavellthedev+github@gmail.com>
2024-10-15 01:37:40 -0400

    worked on JS simple functions also added fat arrow versions

--- Leon/class13-materials/simple-functions/js/main.js
+++ Leon/class13-materials/simple-functions/js/main.js
@@ -1,5 +1,31 @@
 //create a function that adds two numbers and alerts the sum
+// function sumOne(num1, num2) {
+//     let sum = num1 + num2;
+//     return sum
+// }
+
+//FAT ARROW FUNCTION 
+
+let sumOne = (a,b) => a + b; 
+
+alert(sumOne);
 
 //create a function that multiplys three numbers and console logs the product
 
+// function multiplys(a,b,c){
+//     let product = a * b * c;
+//     return product;
+// }
+
+let multiplys = (a,b,c) => a * b * c;
+
+console.log(multiplys(2,3,4));
+
 //create a function that divides two numbers and returns the ???
+
+// function divide(a,b){
+    //   let division = a / b;
+    //   return division;
+    // }
+    
+    // console.log(divide(10,5))
