4113d22b8421655af96d12467817dfa66ced6ddd
Lavell Anthony Wesley <lavellthedev+github@gmail.com>
2024-10-31 11:55:53 -0500

    js practice

--- Leon/class13-materials/bring-it-home-practice/js/main.js
+++ Leon/class13-materials/bring-it-home-practice/js/main.js
@@ -1,13 +1,26 @@
 // *Variables*
 // Create a variable and console log the value
-
+let v = 10;
+console.log(v);
 // Create a variable, add 10 to it, and alert the value
+let b = 10 
+console.log(v+b)
 
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
+ function sub(a,b,c,d){
+    let dif = a-b-c-d;
+    return dif
+ }
 
+ alert(sub(4,3,2,1))
 // Create a function that divides one number by another and returns the remainder
+function div(a,b){
+    let dif = a % b;
+    return dif
+ }
 
+ console.log(div(10,3))
 // *Conditionals*
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
 
