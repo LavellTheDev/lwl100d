c9c7370104b2e9c4b6adef4f64f6e647f392997e
Lavell Anthony Wesley <lavellthedev+github@gmail.com>
2024-11-09 20:57:24 -0500

    homework

--- Leon/class14-materials/temperature-converter/js/main.js
+++ Leon/class14-materials/temperature-converter/js/main.js
@@ -1,1 +1,18 @@
 //Write your pseduo code first! 
+//I need to know the conversion from F to C
+//I need to convert F to C
+//I need a place to input F or C numbers
+//I need a place to display the answer
+
+
+document.querySelector('#yell').addEventListener('click', temp)
+
+function temp() {
+
+  let farhnheit = document.querySelector('#tempF').value
+
+  farhnheit = farhnheit * 9/5 + 32;
+
+  //Add what you should be doing - conditionals go here
+  document.querySelector('#placeToYell').innerText = `${farhnheit}`
+}
