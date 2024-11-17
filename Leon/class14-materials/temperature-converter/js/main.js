c029ac65d65d85d63efeaf0e1c5b54a23612861e
Lavell Anthony Wesley <lavellthedev+github@gmail.com>
2024-11-16 22:00:41 -0500

    added converted C and F

--- Leon/class14-materials/temperature-converter/js/main.js
+++ Leon/class14-materials/temperature-converter/js/main.js
@@ -5,13 +5,30 @@
 //I need a place to display the answer
 
 
+//I need to figure out what is causing only one to appear
+//I need to figure out how to allow only one at a time.
+
 document.querySelector('#yell').addEventListener('click', temp)
+document.querySelector('#yell').addEventListener('click', tempCel)
+
+
+
+
+function tempCel() {
+
+  let celcius = document.querySelector('#tempC').value
+
+  celcius = (celcius * 9/5) + 32;
+
+
+  //Add what you should be doing - conditionals go here
+  document.querySelector('#placeToYellTwo').innerText = `${celcius}`
+}
 
 function temp() {
 
   let farhnheit = document.querySelector('#tempF').value
-
-  farhnheit = farhnheit * 9/5 + 32;
+  farhnheit = (farhnheit - 32) * 5/9;
 
   //Add what you should be doing - conditionals go here
   document.querySelector('#placeToYell').innerText = `${farhnheit}`
