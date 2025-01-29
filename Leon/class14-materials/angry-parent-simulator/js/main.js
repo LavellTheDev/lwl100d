56acbbe9edd21555274006bcd94d7aeb3578932f
Lavell Anthony Wesley <lavellthedev+github@gmail.com>
2025-01-29 08:23:54 -0500

    rewriting js

--- Leon/class14-materials/angry-parent-simulator/js/main.js
+++ Leon/class14-materials/angry-parent-simulator/js/main.js
@@ -13,6 +13,31 @@ function run() {
   document.querySelector('#placeToYell').innerText = `${fName} ${fMidName} ${lMidName} ${lName}`
 }
 
+
+// document.querySelector('#yell').addEventListener('click', run)
+
+// function run(){
+//   const fName = document.querySelector('#firstName').value
+//   const fMidName = document.querySelector('#firstMiddle').value
+//   const lMidName = document.querySelector('#lastName').value
+//   const lName = document.querySelector('#lastName').value
+
+//   document.querySelector('#palceToYell').innerText = `${fname} ${fMidName} ${lMidName} ${lName}`
+// }
+
+
+// document.querySelector('#yell').addEventListener('click', run)
+
+// function run(){
+//   const fName = document.querySelector('#firtName').value 
+//   const fMidName = document.querySelector('#firstMiddle').value
+//   const lMidName = document.querySelector('#lastMiddle').value
+//   const lName = document.querySelector('#lastName').value
+
+//   document.querySelector('#placeToYell').innerText = `${fname} ${fMidName} ${lMidName} ${lName}`
+// }
+
+
 // const synth = window.speechSynthesis;
 // document.querySelector('#yell').addEventListener('click', run)
 //
