c0ea10c19e499d2ad9c81e1b93c6204a6e7a9aac
Lavell Anthony Wesley <lavellthedev+github@gmail.com>
2025-04-07 07:14:21 -0400

    class 16

--- Leon/class14-materials/class-weekend-boring/js/main.js
+++ Leon/class14-materials/class-weekend-boring/js/main.js
@@ -15,3 +15,33 @@ function check() {
   }
 
 }
+
+// document.querySelector('#check').addEventListener('click', check)
+
+// function check (){
+//   const day = document.querySelector('#day').value.toLowerCase()
+
+//   if(day === "tuesday" || day === "thursday"){
+//     console.log("You Have Class")
+//   }else if(day === "saturday" || day === "sunday"){
+//     console.log("Its the Weekend!")
+//   }else{
+//     console.log("Boring")
+//   }
+// }
+
+
+document.querySelector('#check').addEventListener('click', check)
+
+function check(){
+  const day = document.querySelector('#day').value.toLowerCase()
+
+  if(day === "tuesday"|| day === "thursday"){
+    console.log("Today is class")
+  }else if(day === "saturday" || day === "sunday"){
+    console.log("It's the weekend")
+  }else{
+    console.log("Boring")
+  }
+
+}
