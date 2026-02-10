dcf941f7fd27df8de482b74fe422b2a696c442ee
Lavell Anthony Wesley <lavellthedev+github@gmail.com>
2026-02-09 20:58:57 -0600

    studying

--- Leon/class12-materials/background-picker/js/main.js
+++ Leon/class12-materials/background-picker/js/main.js
@@ -107,3 +107,9 @@
 //   document.querySelector('body').style.backgroundColor = 'rgba(0,254,255)'
 //   document.querySelector('body').style.color = 'white'
 // }
+
+document.getElementById('purple').onclick = partyPurple
+
+function partyPurple(){
+    document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
+}
--- Leon/class12-materials/class-weekend-boring/js/main.js
+++ Leon/class12-materials/class-weekend-boring/js/main.js
@@ -1,10 +1,45 @@
-document.querySelector('#check').addEventListener('click', check)
+// document.querySelector('#check').addEventListener('click', check)
 
-function check() {
+// function check() {
 
-  const day = document.querySelector('#day').value
+//   const day = document.querySelector('#day').value
+ 
+//   //Conditionals go here
+//  if (day === "Tuesday" || day === "Thursday") {
+//   alert('Class Day');
+//  } else if (day === "Monday" || day === "Wednesday") {
+//   alert('Boring');
+//  } else {
+//   alert('Weekend');
+//  }
+// }
 
-  //Conditionals go here
 
+// document.querySelector('#check').addEventListener('click', check)
 
+// function check() {
+//   const day = document.querySelector('#day').value;
+
+//   if (day === "Tuesday" || day === "Thursday") {
+//     alert('Class Day');
+//   } else if (day === "Saturday" || day === "Sunday") {
+//     alert('Weekend!');
+//   }else{
+//     alert('Boring');
+//   }
+
+// }
+
+document.querySelector('#check').addEventListener('click', check);
+
+function check(){
+  const day = document.querySelector('#day').value;
+
+  if (day === "Tuesday" || day === "Thursday") {
+    alert("Class day!");
+  } else if(day === "Saturday" || day=== "Sunday") {
+    alert("Weekend!")
+  }else {
+    alert("Boring");
+  }
 }
